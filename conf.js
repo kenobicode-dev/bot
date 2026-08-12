1d2c15eac180e7cf9e8def2b445625fe8eaf24f0
m.pisarev <m.pisarev@smticc.ru>
2026-08-12 20:00:06 +0300

    add

--- conf.js
+++ conf.js
@@ -4,7 +4,7 @@ export const conf = {
         client: "mysql2",
         connection: {
             host: "127.0.0.1",
-            user: "adminbot",
+            user: "root",
             password: "samaellilith",
             database: "my_store"
         }
